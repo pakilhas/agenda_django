4fbd9e2159cca2c312a7167b8e895149cbd61376
Pablo Carvalho <pakilhas@gmail.com>
2023-03-07 23:03:48 -0300

    problema das url resolvido

--- agenda/urls.py
+++ agenda/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
     path('agenda/lista/<int:id_usuario>/', views.json_lista_evento),
-    path('agenda/evento', views.evento),
+    path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
     path('', RedirectView.as_view(url='/agenda/')),
     path('login/',views.login_user ),
